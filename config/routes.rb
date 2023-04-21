38c108c4afe714daae19b059070bed79a0a0c377
elarhadu <elarhadu@gmail.com>
2023-04-21 14:13:43 +0000

    Add routes for shopping list

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,9 @@ Rails.application.routes.draw do
   
   root 'foods#index'
   resources :foods
-   resources :recipes
-  resources :recipe_foods
   resources :recipes do 
     resources :recipe_foods
   end
   get '/public_recipes', to: 'recipes#public_recipes', as: 'public_recipes'
-
+get '/shopping_list', to: 'shopping_list#index', as: 'shopping_list'
 end
