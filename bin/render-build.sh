40bd4801ad02d42251582c8996e745b8c0df40cd
elarhadu <elarhadu@gmail.com>
2023-04-28 12:47:41 +0000

    Modify file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,11 @@
 # exit on error
 set -o errexit
 
+# Install Ruby 3.2.1 using rbenv
+rbenv install 3.2.1
+rbenv local 3.2.1
+
+# Install dependencies and run build commands
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
